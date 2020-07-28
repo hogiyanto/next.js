3f7ca03fd4b934c73c23fa256c340fe7cbbadc35
Hogiyanto Purnama Lucman <hogiyanto.p.lucman@GDPL-LTP-783.local>
2020-07-28 15:21:53 +0700

    implement navigate pages, asset, metadata, and css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,24 @@
+import Link from "next/link";
+import Head from "next/head";
+
 const HomePage = () => {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Read{" "}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
+        </h1>
+      </main>
+    </div>
+  );
 };
 
 export default HomePage;
